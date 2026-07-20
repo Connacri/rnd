0392893378d7c5c666f4736e7b38dd16fbcbbcd0
Ramzi <connacri@gmail.com>
2026-07-20 12:17:13 +0100

    chore: upgrade dependencies and fix localization package path imports

--- lib/core/services/translation_service.dart
+++ lib/core/services/translation_service.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rnd_campaign_app/l10n/app_localizations.dart';
 
 class TranslationService {
   static AppLocalizations? _localizations;
